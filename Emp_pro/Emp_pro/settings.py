eab55efbbd442ff16d75a0457fe541abead35e31
Ramprasath37 <sramprasath37@gmail.com>
2024-06-10 15:01:32 +0530

    vecel add

--- Emp_pro/Emp_pro/settings.py
+++ Emp_pro/Emp_pro/settings.py
@@ -26,8 +26,8 @@ SECRET_KEY = 'django-insecure-h+8@(qd=c8z5c%18pq%&xvm+uqrxeuc%=qi^^-r2i13d_^de7#
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-# ALLOWED_HOSTS = ['.vercel.app']
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
+# ALLOWED_HOSTS = []
 
 
 
